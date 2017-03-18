b8568645dffc90989009944701f73a56615c348d
jimjones26 <jimjones26@gmail.com>
2017-03-18 14:59:45 -0400

    ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store';
+
 import { AppComponent } from './app.component';
+
+import { rootReducer, IAppState, INITIAL_STATE } from './store'; // < New
+import { CounterActions } from './app.actions'; // <- New
 
 @NgModule({
   declarations: [
@@ -12,9 +17,25 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgReduxModule
   ],
-  providers: [],
+  providers: [CounterActions],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>,
+    devTools: DevToolsExtension) {
+    const storeEnhancers = devTools.isEnabled() ? // <- New
+      [devTools.enhancer()] : // <- New
+      []; // <- New
+    // Tell @angular-redux/store about our rootReducer and our initial state.
+    // It will use this to create a redux store for us and wire up all the
+    // events.
+    ngRedux.configureStore(
+      rootReducer,
+      INITIAL_STATE,
+      [],
+      storeEnhancers);
+  }
+}
